b377e160324ddc44c9747a9a621ed7b8a0c919da
advaitalute <120453759+advaitalute@users.noreply.github.com>
2023-07-14 18:02:01 +0530

    Create Button.svelte

---
+++ Button.svelte
@@ -0,0 +1,32 @@
+<script>
+    let buttonProps = {
+    class:[$$restProps.class]
+    }
+</script>
+    <button on:click 
+        {...buttonProps}>
+            <slot/>
+    </button>
+<style>
+    .admin{
+			height:40px;
+			width:80px;
+			background:#6528F7;
+			 margin-top: 0;
+		
+  text-transform: inherit !important;
+  background-color: black !important;
+  color: white;
+			margin-left:45%;
+    }
+    .user {
+			
+			height:40px;
+			width:80px;
+	
+			 margin-top: 0px;
+  text-transform: inherit !important;
+  background-color: black !important;
+  color: white;
+		}
+</style>
